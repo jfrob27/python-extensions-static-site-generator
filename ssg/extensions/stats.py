e6e6f9b34c757f299f3c9c839069592e893e675a
J.-F. Robitaille <jf_robitaille@hotmail.com>
2023-04-05 21:14:07 -0400

    Correct Stats Extension

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -15,13 +15,12 @@ def start_build():
 @hooks.register("written")
 def written():
     global total_written
-    total_written += 1
+    total_written = total_written + 1
 
 
 @hooks.register("stats")
 def stats():
     final_time = time.time() - start_time
-    if total_written > 0:
-        average = final_time / total_written
-        report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file"
-        print(report.format(total_written, final_time, average))
+    average = final_time / total_written if total_written else 0
+    report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file"
+    print(report.format(total_written, final_time, average))
